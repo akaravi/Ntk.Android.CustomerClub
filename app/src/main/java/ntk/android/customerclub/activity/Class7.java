1a3d4637103b8c683b922648a125c71ea1c7feb9
ixiflutter1313 <mj.parishani@outlook.com>
2021-02-04 10:16:57 +0330

    add name

--- app/src/main/java/ntk/android/customerclub/activity/Class7.java
+++ app/src/main/java/ntk/android/customerclub/activity/Class7.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ import com.google.zxing.WriterException;
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 import ntk.android.base.activity.BaseActivity;
+import ntk.android.base.adapter.BaseRecyclerAdapter;
 import ntk.android.base.utill.FontManager;
 import ntk.android.base.utill.prefrense.Preferences;
 import ntk.android.customerclub.R;
@@ -23,6 +26,7 @@ public class Class7 extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.class7);
+        initSize();
         sefont();
 //        ((TextView) findViewById(R.id.txtToolbar)).setText(getString(R.string.mainCard7));
         String qrCode = Preferences.with(this).appVariableInfo().qrCode();
@@ -40,10 +44,23 @@ public class Class7 extends BaseActivity {
         }
     }
 
+    private void initSize() {
+        FrameLayout frameBack = findViewById(R.id.frameBack);
+        FrameLayout frameFront = findViewById(R.id.frameFront);
+        int screenWidth = BaseRecyclerAdapter.getScreenWidth();
+        ViewGroup.LayoutParams layoutParams = frameBack.getLayoutParams();
+        layoutParams.height = (int) (screenWidth * .65);
+        layoutParams.width = (int) (1.6*screenWidth * .65);
+        frameBack.setLayoutParams(layoutParams);
+        frameFront.setLayoutParams(layoutParams);
+
+    }
+
     private void sefont() {
         ((TextView) findViewById(R.id.txtStore)).setTypeface(FontManager.T1_BOLD_Typeface(this));
         ((TextView) findViewById(R.id.textCardBack)).setTypeface(FontManager.T1_Typeface(this));
         ((TextView) findViewById(R.id.txt2)).setTypeface(FontManager.T1_Typeface(this));
+        ((TextView) findViewById(R.id.textName)).setTypeface(FontManager.T1_Typeface(this));
         ((TextView) findViewById(R.id.txt2)).setTypeface(FontManager.T1_Typeface(this));
         ((TextView) findViewById(R.id.txtTitle)).setTypeface(FontManager.GetTypeface(this, "fonts/number7.otf"));
     }
